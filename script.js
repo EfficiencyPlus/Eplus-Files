1b069930f53203f83de09863754971ca3c54656c
gomezced <91102667+CarlosGomez30@users.noreply.github.com>
2022-03-21 15:57:12 -0600

    update

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
-
 function myAccFunc1() {
 var x = document.getElementById("demo_especiales");
 if (x.className.indexOf("w3-show") == -1) {
@@ -47,8 +18,8 @@ if (x.className.indexOf("w3-show") == -1) {
 }
 
 function myFunction2() {
-alert("Ordene directamente desde nuestra Galeria o contactanos por mensaje o llamanos y dinos como te gustaria tu pastel, seguro podemos hacer un pastel a tu gusto.");
-}
+  alert("Por el momento esta funcion no esta disponible. Favor de intentar mas tarde.");
+  }
 
 // Open and close sidebar
 function w3_open() {
