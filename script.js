f0ba30b0558759aefb54eaec7433351d719c0e10
gomezced <91102667+CarlosGomez30@users.noreply.github.com>
2022-03-21 16:00:25 -0600

    updated

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
 function myAccFunc1() {
 var x = document.getElementById("demo_especiales");
 if (x.className.indexOf("w3-show") == -1) {
@@ -31,4 +60,5 @@ function w3_close() {
 document.getElementById("mySidebar").style.display = "none";
 document.getElementById("myOverlay").style.display = "none";
 }
+
 
